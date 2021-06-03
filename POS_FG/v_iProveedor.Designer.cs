9527d4da910f5caf58a796c86f730c2d607881f3
Cristhian Morales <cristhian.moralesf28@gmail.com>
2021-06-03 12:46:55 -0600

    ventas listo

--- POS_FG/v_iProveedor.Designer.cs
+++ POS_FG/v_iProveedor.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.pan_Sup = new System.Windows.Forms.Panel();
             this.lbl_NombreV = new System.Windows.Forms.Label();
+            this.pb_Minimizar = new System.Windows.Forms.PictureBox();
+            this.pb_Cerrar = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
@@ -43,12 +45,10 @@
             this.btn_Cancelar = new System.Windows.Forms.Button();
             this.btn_Agregar = new System.Windows.Forms.Button();
             this.btn_Registrar = new System.Windows.Forms.Button();
-            this.pb_Minimizar = new System.Windows.Forms.PictureBox();
-            this.pb_Cerrar = new System.Windows.Forms.PictureBox();
             this.pan_Sup.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Telefonos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Minimizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Cerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Telefonos)).BeginInit();
             this.SuspendLayout();
             // 
             // pan_Sup
@@ -59,9 +59,9 @@
             this.pan_Sup.Controls.Add(this.pb_Cerrar);
             this.pan_Sup.Dock = System.Windows.Forms.DockStyle.Top;
             this.pan_Sup.Location = new System.Drawing.Point(0, 0);
-            this.pan_Sup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pan_Sup.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pan_Sup.Name = "pan_Sup";
-            this.pan_Sup.Size = new System.Drawing.Size(495, 37);
+            this.pan_Sup.Size = new System.Drawing.Size(660, 46);
             this.pan_Sup.TabIndex = 0;
             this.pan_Sup.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pan_Sup_MouseDown);
             // 
@@ -70,99 +70,130 @@
             this.lbl_NombreV.AutoSize = true;
             this.lbl_NombreV.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_NombreV.ForeColor = System.Drawing.Color.White;
-            this.lbl_NombreV.Location = new System.Drawing.Point(9, 6);
-            this.lbl_NombreV.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_NombreV.Location = new System.Drawing.Point(12, 7);
             this.lbl_NombreV.Name = "lbl_NombreV";
-            this.lbl_NombreV.Size = new System.Drawing.Size(102, 25);
+            this.lbl_NombreV.Size = new System.Drawing.Size(129, 29);
             this.lbl_NombreV.TabIndex = 2;
             this.lbl_NombreV.Text = "Proveedor";
             this.lbl_NombreV.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_NombreV_MouseDown);
+            // 
+            // pb_Minimizar
+            // 
+            this.pb_Minimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_Minimizar.Image = global::POS_FG.Properties.Resources.minimizar;
+            this.pb_Minimizar.Location = new System.Drawing.Point(569, 2);
+            this.pb_Minimizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb_Minimizar.Name = "pb_Minimizar";
+            this.pb_Minimizar.Size = new System.Drawing.Size(31, 34);
+            this.pb_Minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_Minimizar.TabIndex = 1;
+            this.pb_Minimizar.TabStop = false;
+            this.pb_Minimizar.Click += new System.EventHandler(this.pb_Minimizar_Click);
+            // 
+            // pb_Cerrar
+            // 
+            this.pb_Cerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_Cerrar.Image = global::POS_FG.Properties.Resources.letra_x;
+            this.pb_Cerrar.Location = new System.Drawing.Point(617, 2);
+            this.pb_Cerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb_Cerrar.Name = "pb_Cerrar";
+            this.pb_Cerrar.Size = new System.Drawing.Size(31, 34);
+            this.pb_Cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_Cerrar.TabIndex = 0;
+            this.pb_Cerrar.TabStop = false;
+            this.pb_Cerrar.Click += new System.EventHandler(this.pb_Cerrar_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 37);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Location = new System.Drawing.Point(0, 46);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(4, 420);
+            this.panel1.Size = new System.Drawing.Size(5, 516);
             this.panel1.TabIndex = 1;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(491, 37);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Location = new System.Drawing.Point(655, 46);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(4, 420);
+            this.panel2.Size = new System.Drawing.Size(5, 516);
             this.panel2.TabIndex = 2;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(4, 453);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel3.Location = new System.Drawing.Point(5, 557);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(487, 4);
+            this.panel3.Size = new System.Drawing.Size(650, 5);
             this.panel3.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(26, 77);
+            this.label1.Location = new System.Drawing.Point(35, 95);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.Size = new System.Drawing.Size(107, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "ID Proveedor";
             // 
             // txt_ID_Proveedor
             // 
             this.txt_ID_Proveedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_ID_Proveedor.Location = new System.Drawing.Point(183, 74);
+            this.txt_ID_Proveedor.Location = new System.Drawing.Point(244, 91);
+            this.txt_ID_Proveedor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_ID_Proveedor.Name = "txt_ID_Proveedor";
-            this.txt_ID_Proveedor.Size = new System.Drawing.Size(100, 23);
+            this.txt_ID_Proveedor.Size = new System.Drawing.Size(132, 26);
             this.txt_ID_Proveedor.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(26, 122);
+            this.label2.Location = new System.Drawing.Point(35, 150);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(151, 17);
+            this.label2.Size = new System.Drawing.Size(176, 20);
             this.label2.TabIndex = 22;
             this.label2.Text = "Nombre del Proveedor";
             // 
             // txt_NomProveedor
             // 
             this.txt_NomProveedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_NomProveedor.Location = new System.Drawing.Point(183, 119);
+            this.txt_NomProveedor.Location = new System.Drawing.Point(244, 146);
+            this.txt_NomProveedor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_NomProveedor.Name = "txt_NomProveedor";
-            this.txt_NomProveedor.Size = new System.Drawing.Size(186, 23);
+            this.txt_NomProveedor.Size = new System.Drawing.Size(247, 26);
             this.txt_NomProveedor.TabIndex = 23;
             // 
             // dtgv_Telefonos
             // 
             this.dtgv_Telefonos.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dtgv_Telefonos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgv_Telefonos.Location = new System.Drawing.Point(29, 217);
+            this.dtgv_Telefonos.Location = new System.Drawing.Point(39, 345);
+            this.dtgv_Telefonos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtgv_Telefonos.Name = "dtgv_Telefonos";
             this.dtgv_Telefonos.RowHeadersVisible = false;
             this.dtgv_Telefonos.RowHeadersWidth = 51;
             this.dtgv_Telefonos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dtgv_Telefonos.Size = new System.Drawing.Size(240, 150);
+            this.dtgv_Telefonos.Size = new System.Drawing.Size(581, 185);
             this.dtgv_Telefonos.TabIndex = 24;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(26, 168);
+            this.label3.Location = new System.Drawing.Point(35, 207);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.Size = new System.Drawing.Size(73, 20);
             this.label3.TabIndex = 25;
             this.label3.Text = "Telefono";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -170,9 +201,10 @@
             // txt_Telefono
             // 
             this.txt_Telefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_Telefono.Location = new System.Drawing.Point(183, 165);
+            this.txt_Telefono.Location = new System.Drawing.Point(244, 203);
+            this.txt_Telefono.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_Telefono.Name = "txt_Telefono";
-            this.txt_Telefono.Size = new System.Drawing.Size(100, 23);
+            this.txt_Telefono.Size = new System.Drawing.Size(132, 26);
             this.txt_Telefono.TabIndex = 26;
             this.txt_Telefono.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -184,9 +216,11 @@
             this.btn_Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Cancelar.ForeColor = System.Drawing.Color.White;
             this.btn_Cancelar.Image = global::POS_FG.Properties.Resources.cerrar_el_simbolo_de_la_cruz_en_un_circulo;
-            this.btn_Cancelar.Location = new System.Drawing.Point(313, 283);
+            this.btn_Cancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Cancelar.Location = new System.Drawing.Point(39, 287);
+            this.btn_Cancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Cancelar.Name = "btn_Cancelar";
-            this.btn_Cancelar.Size = new System.Drawing.Size(118, 32);
+            this.btn_Cancelar.Size = new System.Drawing.Size(157, 40);
             this.btn_Cancelar.TabIndex = 28;
             this.btn_Cancelar.Text = "Cancelar";
             this.btn_Cancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
@@ -201,9 +235,10 @@
             this.btn_Agregar.ForeColor = System.Drawing.Color.White;
             this.btn_Agregar.Image = global::POS_FG.Properties.Resources.telefono;
             this.btn_Agregar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btn_Agregar.Location = new System.Drawing.Point(313, 244);
+            this.btn_Agregar.Location = new System.Drawing.Point(420, 198);
+            this.btn_Agregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Agregar.Name = "btn_Agregar";
-            this.btn_Agregar.Size = new System.Drawing.Size(118, 32);
+            this.btn_Agregar.Size = new System.Drawing.Size(157, 40);
             this.btn_Agregar.TabIndex = 27;
             this.btn_Agregar.Text = "Agregar ";
             this.btn_Agregar.UseVisualStyleBackColor = false;
@@ -217,46 +252,21 @@
             this.btn_Registrar.ForeColor = System.Drawing.Color.White;
             this.btn_Registrar.Image = global::POS_FG.Properties.Resources.boton_marcado;
             this.btn_Registrar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btn_Registrar.Location = new System.Drawing.Point(313, 321);
+            this.btn_Registrar.Location = new System.Drawing.Point(244, 287);
+            this.btn_Registrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Registrar.Name = "btn_Registrar";
-            this.btn_Registrar.Size = new System.Drawing.Size(118, 32);
+            this.btn_Registrar.Size = new System.Drawing.Size(157, 40);
             this.btn_Registrar.TabIndex = 21;
             this.btn_Registrar.Text = "Registrar";
             this.btn_Registrar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.btn_Registrar.UseVisualStyleBackColor = false;
             // 
-            // pb_Minimizar
-            // 
-            this.pb_Minimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.pb_Minimizar.Image = global::POS_FG.Properties.Resources.minimizar;
-            this.pb_Minimizar.Location = new System.Drawing.Point(427, 2);
-            this.pb_Minimizar.Margin = new System.Windows.Forms.Padding(2);
-            this.pb_Minimizar.Name = "pb_Minimizar";
-            this.pb_Minimizar.Size = new System.Drawing.Size(23, 28);
-            this.pb_Minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pb_Minimizar.TabIndex = 1;
-            this.pb_Minimizar.TabStop = false;
-            this.pb_Minimizar.Click += new System.EventHandler(this.pb_Minimizar_Click);
-            // 
-            // pb_Cerrar
-            // 
-            this.pb_Cerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.pb_Cerrar.Image = global::POS_FG.Properties.Resources.letra_x;
-            this.pb_Cerrar.Location = new System.Drawing.Point(463, 2);
-            this.pb_Cerrar.Margin = new System.Windows.Forms.Padding(2);
-            this.pb_Cerrar.Name = "pb_Cerrar";
-            this.pb_Cerrar.Size = new System.Drawing.Size(23, 28);
-            this.pb_Cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pb_Cerrar.TabIndex = 0;
-            this.pb_Cerrar.TabStop = false;
-            this.pb_Cerrar.Click += new System.EventHandler(this.pb_Cerrar_Click);
-            // 
             // v_iProveedor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(495, 457);
+            this.ClientSize = new System.Drawing.Size(660, 562);
             this.Controls.Add(this.btn_Cancelar);
             this.Controls.Add(this.btn_Agregar);
             this.Controls.Add(this.txt_Telefono);
@@ -272,15 +282,15 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pan_Sup);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "v_iProveedor";
             this.Text = "v_Ventas";
             this.Load += new System.EventHandler(this.v_proveedor_Load);
             this.pan_Sup.ResumeLayout(false);
             this.pan_Sup.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Telefonos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Minimizar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Cerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Telefonos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
