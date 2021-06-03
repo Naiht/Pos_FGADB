3804fc21b4bf8c1f580535ddfbdf094f28490256
Cristhian Morales <cristhian.moralesf28@gmail.com>
2021-06-03 14:33:25 -0600

    nuevo

---
+++ POS_FG/v_VentasCli.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_FG
+{
+    public partial class v_VentasCli : Form
+    {
+        public v_VentasCli()
+        {
+            InitializeComponent();
+        }
+    }
+}
