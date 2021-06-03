e4acd8acd980e99b10f00eebad242a8e51afa9ac
Cristhian Morales <cristhian.moralesf28@gmail.com>
2021-06-03 14:38:13 -0600

    form clientes

--- POS_FG/v_VentasCli.Designer.cs
+++ POS_FG/v_VentasCli.Designer.cs
@@ -28,12 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pan_Sup = new System.Windows.Forms.Panel();
+            this.lbl_NombreV = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dtgv_Clientes = new System.Windows.Forms.DataGridView();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.btn_Registrar = new System.Windows.Forms.Button();
+            this.pb_Cerrar = new System.Windows.Forms.PictureBox();
+            this.pan_Sup.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Clientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Cerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pan_Sup
+            // 
+            this.pan_Sup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
+            this.pan_Sup.Controls.Add(this.lbl_NombreV);
+            this.pan_Sup.Controls.Add(this.pb_Cerrar);
+            this.pan_Sup.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pan_Sup.Location = new System.Drawing.Point(0, 0);
+            this.pan_Sup.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pan_Sup.Name = "pan_Sup";
+            this.pan_Sup.Size = new System.Drawing.Size(696, 46);
+            this.pan_Sup.TabIndex = 0;
+            this.pan_Sup.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pan_Sup_MouseDown);
+            // 
+            // lbl_NombreV
+            // 
+            this.lbl_NombreV.AutoSize = true;
+            this.lbl_NombreV.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_NombreV.ForeColor = System.Drawing.Color.White;
+            this.lbl_NombreV.Location = new System.Drawing.Point(12, 7);
+            this.lbl_NombreV.Name = "lbl_NombreV";
+            this.lbl_NombreV.Size = new System.Drawing.Size(105, 29);
+            this.lbl_NombreV.TabIndex = 2;
+            this.lbl_NombreV.Text = "Clientes";
+            this.lbl_NombreV.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_NombreV_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 46);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 342);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(691, 46);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 342);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(5, 383);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(686, 5);
+            this.panel3.TabIndex = 2;
+            // 
+            // dtgv_Clientes
+            // 
+            this.dtgv_Clientes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dtgv_Clientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv_Clientes.Location = new System.Drawing.Point(28, 82);
+            this.dtgv_Clientes.Margin = new System.Windows.Forms.Padding(4);
+            this.dtgv_Clientes.Name = "dtgv_Clientes";
+            this.dtgv_Clientes.RowHeadersVisible = false;
+            this.dtgv_Clientes.RowHeadersWidth = 51;
+            this.dtgv_Clientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgv_Clientes.Size = new System.Drawing.Size(638, 185);
+            this.dtgv_Clientes.TabIndex = 24;
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.BackColor = System.Drawing.Color.OrangeRed;
+            this.btn_Cancelar.FlatAppearance.BorderSize = 0;
+            this.btn_Cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cancelar.ForeColor = System.Drawing.Color.White;
+            this.btn_Cancelar.Image = global::POS_FG.Properties.Resources.cerrar_el_simbolo_de_la_cruz_en_un_circulo;
+            this.btn_Cancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Cancelar.Location = new System.Drawing.Point(140, 302);
+            this.btn_Cancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(157, 40);
+            this.btn_Cancelar.TabIndex = 28;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn_Cancelar.UseVisualStyleBackColor = false;
+            // 
+            // btn_Registrar
+            // 
+            this.btn_Registrar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btn_Registrar.FlatAppearance.BorderSize = 0;
+            this.btn_Registrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Registrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Registrar.ForeColor = System.Drawing.Color.White;
+            this.btn_Registrar.Image = global::POS_FG.Properties.Resources.boton_marcado;
+            this.btn_Registrar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Registrar.Location = new System.Drawing.Point(409, 302);
+            this.btn_Registrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Registrar.Name = "btn_Registrar";
+            this.btn_Registrar.Size = new System.Drawing.Size(157, 40);
+            this.btn_Registrar.TabIndex = 21;
+            this.btn_Registrar.Text = "Aceptar";
+            this.btn_Registrar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn_Registrar.UseVisualStyleBackColor = false;
+            // 
+            // pb_Cerrar
+            // 
+            this.pb_Cerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_Cerrar.Image = global::POS_FG.Properties.Resources.letra_x;
+            this.pb_Cerrar.Location = new System.Drawing.Point(653, 2);
+            this.pb_Cerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb_Cerrar.Name = "pb_Cerrar";
+            this.pb_Cerrar.Size = new System.Drawing.Size(31, 34);
+            this.pb_Cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_Cerrar.TabIndex = 0;
+            this.pb_Cerrar.TabStop = false;
+            this.pb_Cerrar.Click += new System.EventHandler(this.pb_Cerrar_Click);
+            // 
+            // v_VentasCli
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "v_VentasCli";
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(696, 388);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.dtgv_Clientes);
+            this.Controls.Add(this.btn_Registrar);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pan_Sup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "v_VentasCli";
+            this.Text = "v_Ventas";
+            this.Load += new System.EventHandler(this.v_proveedor_Load);
+            this.pan_Sup.ResumeLayout(false);
+            this.pan_Sup.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Clientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Cerrar)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pan_Sup;
+        private System.Windows.Forms.PictureBox pb_Cerrar;
+        private System.Windows.Forms.Label lbl_NombreV;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btn_Registrar;
+        private System.Windows.Forms.DataGridView dtgv_Clientes;
+        private System.Windows.Forms.Button btn_Cancelar;
     }
 }
--- POS_FG/v_VentasCli.cs
+++ POS_FG/v_VentasCli.cs
@@ -7,14 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace POS_FG
 {
     public partial class v_VentasCli : Form
     {
+        #region MouseDragger
+        //Activa el movimiento de la ventana con el panel
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void pan_Sup_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void lbl_NombreV_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        #endregion
+
+
         public v_VentasCli()
         {
             InitializeComponent();
+        }
+
+        private void pb_Cerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pb_Minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void v_proveedor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
